e7d287bd0fdabea1700bb5e7c214f1f5747beb1d
Nicolas Duque <92602576+Nicolasduquee@users.noreply.github.com>
2023-03-10 22:48:16 -0300

    exercicios

---
+++ ex1/script.js
@@ -0,0 +1,14 @@
+alert("Esse é um programa para calcular o perímetro e área de um círculo.");
+
+var r = parseFloat(prompt("Digite o raio do círculo."));
+
+var c = 2 * 3.14 * r;
+
+var a = 3.14 * (r * r);
+
+var msg = alert('Um circulo com raio de ' + r + ' metros, tem o perímetro de ' + c + ' metros e área de ' + a + ' metros quadrados' );
+
+
+
+
+
